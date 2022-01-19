02416865c8fef0998af314e4f2077de144c2e380
Sir-Hedgehog <mailto:quaresma_08@mail.ru>
2022-01-19 23:15:01 +0300

    First program

--- src/main/kotlin/ru/job4j/hello/HelloKotlin.kt
+++ src/main/kotlin/ru/job4j/hello/HelloKotlin.kt
@@ -1,4 +1,4 @@
-package ru.job4j
+package ru.job4j.hello
 
 /**
  * @author Sir-Hedgehog (mailto:quaresma_08@mail.ru)
--- src/test/java/ru/job4j/hello/HelloJavaTest.java
+++ src/test/java/ru/job4j/hello/HelloJavaTest.java
@@ -1,4 +1,4 @@
-package ru.job4j;
+package ru.job4j.hello;
 
 import org.junit.jupiter.api.Test;
 
